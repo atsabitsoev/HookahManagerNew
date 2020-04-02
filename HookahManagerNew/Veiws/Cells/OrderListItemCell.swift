4765d91071f3485e53b3acf58a42b5cc75fe6003
Ацамаз Бицоев <atsamazbitsoev@yandex.ru>
2020-04-03 00:23:34 +0300

    OrderList Module Configuration

---
+++ HookahManagerNew/Veiws/Cells/OrderListItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderListItemCell.swift
+//  HookahManagerNew
+//
+//  Created by Ацамаз Бицоев on 02.04.2020.
+//  Copyright © 2020 Ацамаз Бицоев. All rights reserved.
+//
+
+import UIKit
+
+class OrderListItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ HookahManagerNew/Veiws/Entities/OrderListItem.swift
@@ -0,0 +1,9 @@
+//
+//  OrderListItem.swift
+//  HookahManagerNew
+//
+//  Created by Ацамаз Бицоев on 02.04.2020.
+//  Copyright © 2020 Ацамаз Бицоев. All rights reserved.
+//
+
+import Foundation
